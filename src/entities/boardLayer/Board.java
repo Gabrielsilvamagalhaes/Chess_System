50a0b3b9e1b1c40b3d7c12da506cda3e6740e0e8
Gabriel Silva <gabiles278@gmail.com>
2023-05-21 19:54:31 -0300

    Adicionando exececoes

--- src/entities/boardLayer/Board.java
+++ src/entities/boardLayer/Board.java
@@ -1,40 +1,54 @@
 package entities.boardLayer;
 
+import exceptions.BoardException;
+
 public class Board {
     private int rows;
     private int columns;
     private Piece[][] pieces;
 
-    public Board(){
-        
-    }
-    public Board(int rows, int columns){
-        this.rows = rows;
-        this.columns = columns;
-        pieces = new Piece[rows] [columns];
+    public Board() {
+
     }
 
-    public Piece piece(int row, int column){
+    public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("ERROR CREATING OF BOARD: the board need at least 1 row or 1 column!");
+        }
+
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public Piece piece(int row, int column) {
+        if (!positionExsits(row, column)) {
+            throw new BoardException("ERROR ACCESSING THE PIECE: the wrong column or row!");
+        }
         return pieces[row][column];
     }
-    public Piece piece(Position position){
-        return pieces[position.getRow()] [position.getColumn()];
-    }
     
-    public void removePiece(Position position){
+    public Piece piece(Position position) {
+        if (!positionExsits(position)) {
+            throw new BoardException("ERROR ACCESSING THE PIECE: the wrong column or row!");
+        }
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void removePiece(Position position) {
 
         Piece[][] newPieces = new Piece[pieces.length - 1][pieces[0].length - 1];
 
         int newRow = 0;
         for (int i = 0; i < pieces.length; i++) {
             if (i == position.getRow()) {
-                continue; 
+                continue;
             }
 
             int newColumn = 0;
             for (int j = 0; j < pieces[0].length; j++) {
                 if (j == position.getColumn()) {
-                    continue; 
+                    continue;
                 }
 
                 newPieces[newRow][newColumn] = pieces[i][j];
@@ -44,20 +58,34 @@ public class Board {
             newRow++;
         }
 
-
     }
 
-    public void placePiece(Piece piece, Position position){
+    public void placePiece(Piece piece, Position position) {
+        if(thereIsAPiece(position)){
+            throw new BoardException("ERROR ACCESSING THE PIECE: the position already exists!");
+
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
 
     }
 
+    private boolean positionExsits(int row, int column) {
+        return column >= 0 && row >= 0 && column < 8 && row < 8;
 
+    }
 
+    public boolean positionExsits(Position position) {
+        return positionExsits(position.getRow(), position.getColumn());
+    }
 
+    public boolean thereIsAPiece(Position position) {
+        if(!positionExsits(position)){
+            throw new BoardException("ERROR ACCESSING THE PIECE: the wrong column or row!");
+        }
 
-    
+        return piece(position) != null;
+    }
 
     /**
      * @return int return the rows
@@ -67,19 +95,10 @@ public class Board {
     }
 
     /**
-     * @param rows the rows to set
-     */
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    /**
      * @return int return the columns
      */
     public int getColumns() {
         return columns;
     }
-
-    
 
 }
